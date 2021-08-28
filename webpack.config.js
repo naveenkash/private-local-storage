862357404b67f924f1f312f5bfa2654792610f9b
naveenkash <naveenkashyap243@gmail.com>
2021-08-29 01:43:46 +0530

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
   watch: process.env.NODE_ENV === "production" ? false : true,
   entry: path.resolve(__dirname, "./src/index.js"),
+  target: "web",
   output: {
     path: path.resolve(__dirname, "./lib"),
-    libraryTarget: "commonjs2",
-    filename: "index.js",
+    libraryTarget: "umd",
+    filename: "app.bundle.js",
+    umdNamedDefine: true,
+    globalObject: "this",
   },
   module: {
     rules: [
